5713482e41b4ae16d3a2c7daff6bc8b3dd14c29d
Ivanpepp <nastroivan3@gmail.com>
2021-08-26 09:57:16 +0200

    typo fixed

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 
-for (i = 0; i < 100; i++){
+for (let i = 1; i <= 100; i++){
    
     if ((i % 3 == 0) && (i % 5 == 0)){
         console.log('FizzBuzz');
@@ -7,5 +7,7 @@ for (i = 0; i < 100; i++){
         console.log('Fizz');
     }else if (i % 5 == 0){
         console.log('Buzz');
+    } else {
+        console.log(i);
     }
 }
